8e435af85c2d8df92ecc5a1f58f69b680e0978e4
jcao91 <jcao91@yahoo.com>
2024-06-11 11:16:03 -0500

    .ejs/.js dir and files made

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config();
 const express = require("express");
 const app = express();
 const session = require("express-session");
-const methodOverride = require("method-overide");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const path = require("path");
@@ -13,4 +13,19 @@ const port = process.env.PORT ? process.env.PORT : "3000";
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+
+
+
+
+
+
+
+
+
+
+  
 });
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+})
