befa14004fcaa3d3e183aadb1f19a3aaba4b8ba4
jcao91 <jcao91@yahoo.com>
2024-06-16 21:20:43 -0500

    corrected error in user controller to display correct show page

--- controllers/users.js
+++ controllers/users.js
@@ -13,8 +13,7 @@ const index = async (req, res) => {
 const show = async (req, res) => {
     try {
       const user = await User.findById(req.params.userId);
-      const currentUser = await User.findById(req.session.user._id);
-      res.render("users/show.ejs", { user, triathlon: currentUser.triathlon } );
+      res.render("users/show.ejs", { user, triathlon: user.triathlon } );
     } catch (error) {
       console.error(error);
       res.redirect("/");
